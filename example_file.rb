df7e24c547f0cbfbb48fc15eb3528fee5184b67a
Eric Uberman <euberman@gmail.com>
2020-12-17 22:54:55 +0000

    Automatically backed up by Learn

--- example_file.rb
+++ example_file.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 vending_array = [[[{:name=>"Vanilla Cookies", :price=>3},{:name=>"Pistachio Cookies", :price=>3},{:name=>"Chocolate Cookies", :price=>3},{:name=>"Chocolate Chip Cookies", :price=>3}],[{:name=>"Tooth-Melters", :price=>12},{:name=>"Tooth-Destroyers", :price=>12},{:name=>"Enamel Eaters", :price=>12},{:name=>"Dentist's Nightmare", :price=>20}],[{:name=>"Gummy Sour Apple", :price=>3},{:name=>"Gummy Apple", :price=>5},{:name=>"Gummy Moldy Apple", :price=>1}]],[[{:name=>"Grape Drink", :price=>1},{:name=>"Orange Drink", :price=>1},{:name=>"Pineapple Drink", :price=>1}],[{:name=>"Mints", :price=>13},{:name=>"Curiously Toxic Mints", :price=>1000},{:name=>"US Mints", :price=>99}]]]
 
 def sum_nested_array(vending_machine)
@@ -25,3 +26,6 @@ def sum_nested_array(vending_machine)
 end
 
 puts sum_nested_array(vending_array)
+=======
+puts "Hello World"
+>>>>>>> 11f8d983c2b8a2eb0f629f00b84f4c5a7ab4f625
